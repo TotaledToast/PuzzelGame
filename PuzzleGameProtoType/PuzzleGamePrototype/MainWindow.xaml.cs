a43cf6b8a93f7cf3be2dd2657096b5a67c5745d1
TotaledToast <81196042+TotaledToast@users.noreply.github.com>
2022-04-01 12:28:31 +0100

    Started
    very unfinished

---
+++ PuzzleGameProtoType/PuzzleGamePrototype/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PuzzleGamePrototype
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int CurrentSpaceCycle = 0;
+        static List<string> SpaceText = new List<string> {
+            "Just Press Space", "It's really not hard you know", "Seriously Just Press it Like My God", "Jesus I give up", "....."
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space && PressSpace.IsVisible == true)
+            {
+                PressSpace.Visibility = Visibility.Collapsed;
+            }
+            else if (PressSpace.IsVisible == true)
+            {
+                if (CurrentSpaceCycle < 5)
+                {
+                    PressSpace.Text = SpaceText[CurrentSpaceCycle];
+                    CurrentSpaceCycle++;
+                }
+                else
+                {
+                    PressSpace.Text = PressSpace.Text + ".";
+                }
+            }
+
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+    public class LoadNewLevel
+    {
+        public static void LoadLevel1()
+        {
+
+        }
+    }
+}
